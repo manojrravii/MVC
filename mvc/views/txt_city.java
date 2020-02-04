fedd662ba84dcdc9a106154072a5eab1bf7f0819
manojrravii <60592708+manojrravii@users.noreply.github.com>
2020-02-04 16:01:16 +0530

    Add files via upload

---
+++ mvc/views/txt_city.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvc.views;
+
+/**
+ *
+ * @author CSE
+ */
+class txt_city {
+
+    static void setText(String city) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
